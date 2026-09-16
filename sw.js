2662da28e00c01afdbc9ecc93ec44064dcdbdcf1
syl20dai-maker <magchoupy2@gmail.com>
2026-09-16 06:02:24 +0200

    Force la mise à jour PWA des points de masse

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='equilibrage-v4';
+const CACHE='equilibrage81-v5-mass-dots';
 const FILES=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
